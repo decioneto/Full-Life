bfb60baaf1896f2f8b363e01bd6616f09a082e16
Décio Neto <decio.neto@tecnologia2u.com.br>
2020-03-12 17:39:37 -0300

    Business and cards

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ $(".nav-link").click(function() {
     $(".nav-link").removeClass("active");
     $(this).addClass("active");
 });
+
+$(window).scroll(function() {
+    let height = $(window).scrollTop();
+    if(height < 100) {
+        $(".nav-link").removeClass("active");
+        $(".nav-link").first().addClass("active");
+    }
+});
